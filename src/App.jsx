1b4ff99587c540158c13b191820b59d53226184b
Ashis Kumar Joy <ashis263@gmail.com>
2025-05-25 00:29:34 +0600

    fixed import issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import AnnouncementBar from "./components/AnnouncementBar/AnnouncementBar";
 import Footer from "./components/Footer/Footer";
 import MainSection from "./components/MainSection/MainSection";
 import Navbar from "./components/Navbar/Navbar";
-import Newsletter from "./components/NewsLetter/Newsletter";
+import Newsletter from "./components/Newsletter/Newsletter"
 import { ProductsProvider } from "./contexts/ProductsContext";
 
 const App = () => {
